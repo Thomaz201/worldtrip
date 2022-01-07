5173f8c17adcf51883f275e3a6e8cef26e13ee80
Lucas <lucasthomazr@hotmail.com>
2022-01-07 15:26:46 -0300

    Created Banner component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,29 @@
+import Head from 'next/head';
+import { Flex } from '@chakra-ui/react';
 import { Header } from '../components/Header';
+import { Banner } from '../components/Banner';
+import { ContinentCarouselModel } from '../types';
 
-export default function Home() {
+interface HomeProps {
+  continents: ContinentCarouselModel[];
+}
+
+export default function Home({ continents }: HomeProps) {
   return (
-    <div>
-      <Header />
-    </div>
+    <>
+      <Head>
+        <title>Home | worldtrip</title>
+        <meta
+          name="description"
+          content="5 Continentes, intinitas possibilidades"
+        />
+        <meta name="image" content="/images/americas/call.png" />
+      </Head>
+
+      <Flex direction="column" h="100vh" w="100%" maxW={1440} mx="auto">
+        <Header />
+        <Banner />
+      </Flex>
+    </>
   );
 }
